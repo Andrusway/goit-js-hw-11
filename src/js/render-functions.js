1ce4c57e3c3b1f854c77d7416a85d4cbd27ba197
Andrii_Petrukh <andriypetruch2004@gmail.com>
2025-09-26 00:28:36 +0300

    fixed

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -16,10 +16,10 @@ const markup =  images.map(({webformatURL, largeImageURL, tags, likes, views, co
 <img class="gallery-image" src="${webformatURL}" alt="${tags}" loading="lazy">
 </a>
 <div class="info">
-<p class"info-item"> <b>Likes</b> <span>${likes}</span></p>
-<p class"info-item"> <b>Views</b> <span>${views}</span></p>
-<p class"info-item"> <b>Comments</b> <span>${comments}</span></p>
-<p class"info-item"> <b>Downloads</b> <span>${downloads}</span></p>
+<p class="info-item"> <b>Likes</b> <span>${likes}</span></p>
+<p class="info-item"> <b>Views</b> <span>${views}</span></p>
+<p class="info-item"> <b>Comments</b> <span>${comments}</span></p>
+<p class="info-item"> <b>Downloads</b> <span>${downloads}</span></p>
 </div>
 </li>
 `).join("")
